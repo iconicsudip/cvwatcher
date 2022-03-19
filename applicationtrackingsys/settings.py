56a322862bec6df62d5eb41befd7bc9c3be91d79
Sudip Das <61116990+iconicsudip@users.noreply.github.com>
2022-03-19 17:33:19 +0530

    Update settings.py

--- applicationtrackingsys/settings.py
+++ applicationtrackingsys/settings.py
@@ -118,6 +118,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL='/media/'
 STATIC_URL = '/static/'
